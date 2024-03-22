21201e9cd23f30e5cf4211f57e2be97acff6ce4d
“Dino” <daringamaleldin@hotmail.com>
2024-03-22 07:30:28 -0700

    update

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Puppet manifest to create a file in /tmp
+
+# Define the file resource
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
