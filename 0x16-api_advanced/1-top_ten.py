808be36ed5fee518bf55309e661d43a2d2ce6ba5
“Dino” <daringamaleldin@hotmail.com>
2024-05-07 12:23:30 -0700

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,44 +1,21 @@
 #!/usr/bin/python3
-"""Function to query the Reddit API and print the titles of the first 10 hot posts for a given subreddit."""
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Print the titles of the first 10 hot posts for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-
-    data = response.json().get("data")
-    if not data:
-        print(None)
-        return
-
-    children = data.get("children")
-    if not children:
-        print(None)
-        return
-
-    for post in children:
-        print(post.get("data").get("title"))
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
