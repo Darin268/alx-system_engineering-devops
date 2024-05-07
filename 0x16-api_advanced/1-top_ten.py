2b156b884a0992468573a412ccb30d3938050766
“Dino” <daringamaleldin@hotmail.com>
2024-05-07 12:33:02 -0700

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""
+This module contains the function top_ten.
+"""
 import requests
-from sys import argv
+import sys
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    """
+    Returns the top ten posts for a given subreddit.
+    """
+    user_agent = {'User-Agent': 'Lizzie'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=user_agent)
+
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        data = response.json().get('data', {}).get('children', [])
+        for post in data:
+            print(post.get('data', {}).get('title'))
     except Exception:
         print(None)
 
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(sys.argv) < 2:
+        print("Usage: {} <subreddit>".format(sys.argv[0]))
+        sys.exit(1)
+    top_ten(sys.argv[1])
